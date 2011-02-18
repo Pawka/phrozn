a1a38c4ebe05eba97c19834502b6c3fe6087e8ef
Victor Farazdagi <simple.square@gmail.com>
2011-02-18 19:23:43 +0300

    Updated runner to user yaml config files

--- bin/phrozn.php
+++ bin/phrozn.php
@@ -7,14 +7,15 @@ use Phrozn\Runner\CommandLine as Runner,
 defined('PHROZN_PATH_APP') or define('PHROZN_PATH_APP', realpath(dirname(__FILE__) . '/../') . '/');
 defined('PHROZN_PATH_BIN') or define('PHROZN_PATH_BIN', PHROZN_PATH_APP . 'bin/');
 defined('PHROZN_PATH_LIB') or define('PHROZN_PATH_LIB', PHROZN_PATH_APP . 'library/');
+defined('PHROZN_PATH_CONFIGS') or define('PHROZN_PATH_CONFIGS', PHROZN_PATH_APP . 'configs/');
 
 // auto-loader
 require_once PHROZN_PATH_LIB . 'Zend/Loader/StandardAutoloader.php';
-$loader = new Autoloader(array(
-    'Zend'      => PHROZN_PATH_LIB . 'Zend',
-    'Phrozn'    => PHROZN_PATH_LIB . 'Phrozn',
-));
+$loader = new Autoloader();
 $loader
+    ->registerNamespace('Zend', PHROZN_PATH_LIB . 'Zend')
+    ->registerNamespace('Phrozn', PHROZN_PATH_LIB . 'Phrozn')
+    ->registerNamespace('Symfony', PHROZN_PATH_LIB . 'Symfony')
     ->setFallbackAutoloader(true)
     ->register();
 
