7ef4927d0ec6c56e3318e7de7ca907b10dd4c63f
Victor Farazdagi <simple.square@gmail.com>
2011-02-18 18:16:22 +0300

    Updated phrozn binary to utilize Zend\Loader

--- bin/phrozn.php
+++ bin/phrozn.php
@@ -1,5 +1,22 @@
 #!/usr/bin/php
 <?php
-require_once 'Phrozn/Runner/CommandLine.php';
-Phrozn_Runner_CommandLine::run();
+namespace Phrozn;
+use Phrozn\Runner\CommandLine as Runner,
+    Zend\Loader\StandardAutoloader as Autoloader;
+
+defined('PHROZN_PATH_APP') or define('PHROZN_PATH_APP', realpath(dirname(__FILE__) . '/../') . '/');
+defined('PHROZN_PATH_BIN') or define('PHROZN_PATH_BIN', PHROZN_PATH_APP . 'bin/');
+defined('PHROZN_PATH_LIB') or define('PHROZN_PATH_LIB', PHROZN_PATH_APP . 'library/');
+
+// auto-loader
+require_once PHROZN_PATH_LIB . 'Zend/Loader/StandardAutoloader.php';
+$loader = new Autoloader(array(
+    'Zend'      => PHROZN_PATH_LIB . 'Zend',
+    'Phrozn'    => PHROZN_PATH_LIB . 'Phrozn',
+));
+$loader
+    ->setFallbackAutoloader(true)
+    ->register();
+
+Runner::run();
 
