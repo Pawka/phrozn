5665939a5f447192271c23e7585802902cdae2a5
Victor Farazdagi <simple.square@gmail.com>
2011-02-23 15:20:10 +0300

    Callback\Up firt UT added

---
+++ tests/Phrozn/Runner/CommandLine/Callback/UpTest.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Runner\CommandLine\Callback
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace PhroznTest\Runner\CommandLine\Callback;
+use Phrozn\Runner\CommandLine\Callback\Up as Callback,
+    Phrozn\Runner\CommandLine as Runner,
+    Phrozn\Runner\CommandLine\Parser;
+
+/**
+ * @category    Phrozn
+ * @package     Phrozn\Runner\CommandLine\Callback
+ * @author      Victor Farazdagi
+ */
+class UpTest 
+    extends \PHPUnit_Framework_TestCase
+{
+    private $runner;
+    private $outputter;
+    private $parser;
+
+    public function setUp()
+    {
+        // paths
+        $base = realpath(dirname(__FILE__) . '/../../../../../') . '/';
+        $paths = array(
+            'app'       => $base,
+            'bin'       => $base . 'bin/',
+            'lib'       => $base . 'library/',
+            'configs'   => $base . 'configs/',
+            'skeleton'  => $base . 'skeleton/',
+        );
+
+        // purge project directory
+        $this->removeProjectDirectory();
+        
+        $this->outputter = new Outputter($this);
+        $runner = new Callback();
+        $data['paths'] = $paths; // inject paths
+        $runner
+            ->setOutputter($this->outputter)
+            ->setConfig($data);
+        $this->runner = $runner;
+
+        // setup parser
+        $this->parser = new Parser($paths);
+    }
+
+    public function tearDown()
+    {
+        // purge project directory
+        $this->removeProjectDirectory();
+    }
+
+    public function testProjectCompile()
+    {
+        $out = $this->outputter;
+
+        $path = dirname(__FILE__) . '/project/subpath';
+        mkdir($path);
+
+        // initialize project
+        $this->assertFalse(is_dir($path . '/_phrozn'));
+        $this->assertFalse(is_dir($path . '/_phrozn/site'));
+        $this->assertFalse(is_readable($path . '/_phrozn/config.yml'));
+        `phr init {$path}`;
+        $this->assertTrue(is_dir($path . '/_phrozn'));
+        $this->assertTrue(is_dir($path . '/_phrozn/site'));
+        $this->assertTrue(is_readable($path . '/_phrozn/config.yml'));
+
+        $result = $this->getParseResult("phr up {$path} {$path}");
+
+        $this->runner
+            ->setParseResult($result)
+            ->execute();
+
+        $out->assertInLogs("[OK]      Source directory located: {$path}/_phrozn");
+        $out->assertInLogs("[OK]      Destination directory located: {$path}/_phrozn/site");
+
+    }
+
+    private function getParseResult($cmd)
+    {
+        $args = explode(' ', $cmd);
+        return $this->parser->parse(count($args), $args);
+    }
+
+    private function removeProjectDirectory()
+    {
+        $path = dirname(__FILE__) . '/project/subpath';
+
+        if (is_dir($path)) {
+            `rm -rf {$path}`;
+        }
+
+        $path .= '/_phrozn';
+        if (is_dir($path)) {
+            `rm -rf {$path}`;
+        }
+    }
+}
