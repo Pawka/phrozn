3afdeb39da673c88262e7b8b6d4fd18249cbb165
Victor Farazdagi <simple.square@gmail.com>
2011-02-23 13:24:46 +0300

    Outputter UT Helper: inject test case and simplify log testing

--- tests/Phrozn/Runner/CommandLine/Callback/Outputter.php
+++ tests/Phrozn/Runner/CommandLine/Callback/Outputter.php
@@ -39,24 +39,76 @@ class Outputter
      */
     private $lines = array();
 
+    /**
+     * Reference to current test case
+     * @var \PHPUnit_Framework_TestCase 
+     */
+    private $testCase;
+
+    public function __construct($testCase)
+    {
+        $this->testCase = $testCase;
+    }
+
+    /**
+     * Add line to output
+     *
+     * @param string $str Line to add
+     *
+     * @return \PhroznTest\Runner\CommandLine\Callback\Outputter
+     */
     public function stdout($str)
     {
         $str = Color::strip(Color::convert($str));
-        $this->lines[] = $str;
+        $this->lines[] = trim($str);
+        return $this;
     }
 
+    /**
+     * Get all outputted lines
+     *
+     * @return array
+     */
     public function getLines()
     {
         return $this->lines;
     }
 
-    public function contains($str)
+    /**
+     * Asserts that the log buffer contains specified message
+     *
+     * @param string $expected Message subsctring
+     * @param string $errmsg The error message to display.
+     *
+     * @return void
+     */
+    public function assertInLogs($expected, $errorMsg = "Expected to find '%s' in logs:\n\n%s")
     {
-        foreach ($this->getLines() as $line) {
-            if (strpos(trim($line), $str) !== false) {
-                return true;
+        foreach($this->getLines() as $log) {
+            if (false !== stripos($log, $expected)) {
+                $this->testCase->assertEquals(1, 1); // increase number of positive assertions
+                return;
             }
         }
-        return false;
+        $this->testCase->fail(sprintf($errorMsg, $expected, var_export($this->getLines(),true)));
+    }
+
+    /**
+     * Asserts that the log buffer does NOT contain specified message
+     *
+     * @param string $expected Message subsctring
+     * @param string $errmsg The error message to display.
+     *
+     * @return void
+     */
+    public function assertNotInLogs($message, $errorMsg = "Unexpected string '%s' found in logs:\n\n%s")
+    {
+        foreach($this->getLines() as $log) {
+            if (false !== stripos($log, $message)) {
+                $this->testCase->fail(sprintf($errorMsg, $message, var_export($this->getLines(), true)));
+            }
+        }
+
+        $this->testCase->assertEquals(1, 1); // increase number of positive assertions
     }
 }
