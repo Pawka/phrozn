4eb35804ecf9e918c049140b7b742c26036333a8
Victor Farazdagi <simple.square@gmail.com>
2011-03-24 06:03:59 +0300

    Added easy pear package release tool

---
+++ bin/release.sh
@@ -0,0 +1,15 @@
+dir=$(
+    cd -P -- "$(dirname -- "$0")" && pwd -P
+)
+dir=$dir"/../"
+cd $dir
+php ./build/build.php
+pear package ./package.xml
+
+#cleanup pirum
+rm -f ./build/pear/get/*
+cd ./build/pear
+pirum build .
+pirum add . ../../*.tgz
+cd $dir
+rm *.tgz
