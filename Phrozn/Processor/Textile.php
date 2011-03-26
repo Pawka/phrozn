39be72803f85da98d26b5cfe25712fd6e5c739d4
Victor Farazdagi <simple.square@gmail.com>
2011-03-27 01:22:03 +0300

    Added textile support

---
+++ Phrozn/Processor/Textile.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace Phrozn\Processor;
+use Phrozn\Autoloader as Loader;
+
+/**
+ * Textile markup processor
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ */
+class Textile
+    extends Base
+    implements \Phrozn\Processor 
+{
+    /**
+     * Reference to procesor class
+     * @var \Textile
+     */
+    private $textile;
+
+    /**
+     * Processor can be setup at initialization time
+     *
+     * @param array $options Processor options
+     *
+     * @return void
+     */
+    public function __construct($options = array())
+    {
+        $path = Loader::getInstance()->getPath('library');
+        require_once $path . '/Vendor/Extra/textile.php';
+        $this->textile = new \Textile;
+    }
+
+    /**
+     * Parse the incoming template
+     *
+     * @param string $tpl Source template content
+     * @param array $vars List of variables passed to template engine
+     *
+     * @return string Processed template
+     */
+    public function render($tpl, $vars = array())
+    {
+        return $this->textile->TextileThis($tpl);
+    }
+}
---
+++ tests/Phrozn/Processor/TextileTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace PhroznTest\Processor;
+use Phrozn\Processor\Textile as Processor;
+
+/**
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ */
+class TextileTest 
+    extends \PHPUnit_Framework_TestCase
+{
+    private $path;
+
+    public function setUp()
+    {
+        $this->path = dirname(__FILE__) . '/templates/';
+    }
+
+    public function testRender()
+    {
+        $processor = new Processor();
+        $tpl = file_get_contents($this->path . 'tpl1.textile');
+        $rendered = $processor->render($tpl);
+        $static = file_get_contents($this->path . 'tpl1.textile.html');
+        $this->assertSame(trim($static), trim($rendered));
+    }
+}
