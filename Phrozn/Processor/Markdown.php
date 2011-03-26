1d821de562b1cdb8fa4d5d433d4df676e8d25cd3
Victor Farazdagi <simple.square@gmail.com>
2011-03-27 00:31:49 +0300

    added markdown support

---
+++ Phrozn/Processor/Markdown.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace Phrozn\Processor;
+use Phrozn\Autoloader as Loader;
+
+/**
+ * Markdown markup processor
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Processor
+ * @author      Victor Farazdagi
+ */
+class Markdown
+    extends Base
+    implements \Phrozn\Processor 
+{
+    /**
+     * Reference to procesor class
+     * @var \Markdown_Parser
+     */
+    private $markdown;
+
+    /**
+     * Processor can be setup at initialization time
+     *
+     * @param array $options Processor options
+     *
+     * @return void
+     */
+    public function __construct($options = array())
+    {
+        $path = Loader::getInstance()->getPath('library');
+
+        // Twig uses perverted file naming (due to absense of NSs at a time it was written)
+        // so fire up its own autoloader
+        require_once $path . '/Vendor/Extra/markdown.php';
+        $this->markdown = new \Markdown_Parser();
+    }
+
+    /**
+     * Parse the incoming template
+     *
+     * @param string $tpl Source template content
+     * @param array $vars List of variables passed to template engine
+     *
+     * @return string Processed template
+     */
+    public function render($tpl, $vars = array())
+    {
+        return $this->markdown->transform($tpl);
+    }
+}
