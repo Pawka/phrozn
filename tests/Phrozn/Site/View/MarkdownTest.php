386184e661aa99d6cc0d12f287b1cb9e9aff8dd7
Victor Farazdagi <simple.square@gmail.com>
2011-04-09 05:28:45 +0400

    View/Markdown UTs added

---
+++ tests/Phrozn/Site/View/MarkdownTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Site\View
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace PhroznTest\Site\View;
+use Phrozn\Site\View\Markdown as View;
+
+/**
+ * @category    Phrozn
+ * @package     Phrozn\Site\View
+ * @author      Victor Farazdagi
+ */
+class MarkdownTest 
+    extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {}
+
+    public function testViewCreation()
+    {
+        $in = dirname(__FILE__) . '/entries/markdown.markdown';
+        $out = dirname(__FILE__) . '/out';
+        $view = new View($in , $out);
+
+        $this->assertInstanceOf('\Phrozn\Site\View\Markdown', $view);
+    }
+
+    public function testViewRendering()
+    {
+        $entry = dirname(__FILE__) . '/entries/markdown.markdown';
+        $html = dirname(__FILE__) . '/entries/markdown.html';
+        $view = new View($entry);
+
+        $rendered = $view->render();
+        $loaded = file_get_contents($html);
+
+        $this->assertSame(trim($loaded), trim($rendered));
+    }
+
+    public function testViewCompiling()
+    {
+        $entry = dirname(__FILE__) . '/entries/markdown.markdown';
+        $html = dirname(__FILE__) . '/entries/markdown.html';
+        $path = dirname(__FILE__) . '/out/'; 
+        $view = new View($entry, $path);
+
+        $this->assertSame('markdown.markdown', basename($view->getInputFile()));
+        $this->assertSame('markdown.html', basename($view->getOutputFile()));
+
+        @unlink($path . 'markdown.html');
+        $this->assertFalse(is_readable($path . 'markdown.html'));
+
+        $rendered = $view->compile();
+
+        $this->assertTrue(is_readable($path . 'markdown.html'));
+
+        $loaded = file_get_contents($html);
+        $this->assertSame(trim($loaded), trim($rendered));
+
+        // load from out
+        $loaded = file_get_contents($path . 'markdown.html');
+        $this->assertSame(trim($loaded), trim($rendered));
+
+        // cleanup
+        unlink($path . 'markdown.html');
+    }
+
+    public function testNoSourcePathSpecified()
+    {
+        $this->setExpectedException('Exception', "View input file not specified");
+        $view = new View();
+
+        $rendered = $view->render(array());
+
+    }
+
+
+}
---
+++ tests/Phrozn/Site/View/entries/markdown.markdown
@@ -0,0 +1,56 @@
+---
+This is a paragraph. It has two sentences.
+
+This is another paragraph. It also has two sentences.
+
+*emphasis* or _emphasis_
+
+**strong emphasis** or __strong emphasis__
+
+Some text with `some code` inside
+
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+* An item in a bulleted (unordered) list
+    * A subitem, indented with 4 spaces
+* Another item in a bulleted list
+
+1. An item in an enumerated (ordered) list
+2. Another item in an enumerated list
+
+# First-level heading
+
+#### Fourth-level heading
+
+First-level heading
+===================
+
+Second-level heading
+--------------------
+
+> This text will be enclosed in an HTML blockquote element.
+> Blockquote elements are reflowable. You may arbitrarily
+> wrap the text to your liking, and it will all be parsed
+> into a single blockquote element.
+
+[link text here](link.address.here "link title here")
+
+[link text here][linkref]
+
+[linkref]: link.address.here "link title here"
+
+![Alt text here](/usr/images/test.png "Image title here")
+
+* * *
+
+***
+
+*****
+
+- - -
+
+---------------------------------------
+
+
