d3f51eecd938d909f69cce0d4ee9c53135d36a9e
Victor Farazdagi <simple.square@gmail.com>
2011-05-05 04:43:28 +0400

    Provider\Base + Provider\LoadFromFile added

---
+++ Phrozn/Provider/Base.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Provider
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace Phrozn\Provider;
+use Phrozn\Provider;
+
+/**
+ * Implementation of common methods of content providers
+ *
+ * @category    Phrozn
+ * @package     Phrozn\Provider
+ * @author      Victor Farazdagi
+ */
+abstract class Base 
+    implements Provider
+{
+    /**
+     * Engine environment configuration
+     */
+    private $config = array();
+
+    /**
+     * Project path
+     * @var string
+     */
+    private $projectPath;
+
+    /**
+     * Init provider
+     *
+     * @param array $options Config options
+     *
+     * @return void
+     */
+    public function __construct($options = null)
+    {
+        $this->setConfig($options);
+    }
+
+    /**
+     * Setup provider
+     *
+     * @param array $options Options to pass to provider
+     *
+     * @return \Phrozn\Provider
+     */
+    public function setConfig($options)
+    {
+        $this->config = $options;
+        return $this;
+    }
+
+    /**
+     * Get provider configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * Set project path.
+     *
+     * @param string $path Project path.
+     *
+     * @return \Phrozn\Has\ProjectPath
+     */
+    public function setProjectPath($path)
+    {
+        $this->projectPath = $path;
+        return $this;
+    }
+
+    /**
+     * Get project path.
+     *
+     * @return string
+     */
+    public function getProjectPath()
+    {
+        return $this->projectPath;
+    }
+
+}
---
+++ tests/Phrozn/Provider/LoadFromFileTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright 2011 Victor Farazdagi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    PhroznTest
+ * @package     Phrozn\Provider
+ * @author      Victor Farazdagi
+ * @copyright   2011 Victor Farazdagi
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace PhroznTest;
+use \PHPUnit_Framework_TestCase as TestCase,
+    Phrozn\Provider\LoadFromFile as Provider;
+
+/**
+ * @category    PhroznTest
+ * @package     Phrozn\Provider
+ * @author      Victor Farazdagi
+ */
+class ProviderTest 
+    extends TestCase
+{
+    public function testProvider()
+    {
+        $input = dirname(__FILE__) . '/LoadFromFile.txt';
+        $options = array(
+            'input' => basename($input),
+        );
+        $provider = new Provider($options);
+        $provider->setProjectPath(dirname($input));
+        $this->assertSame(file_get_contents($input), $provider->get());
+    }
+
+    public function testProviderNoInputSet()
+    {
+        $this->setExpectedException('Exception', 'No input file provided');
+        $provider = new Provider();
+        $provider->get();
+    }
+
+    public function testProviderInvalidInputFile()
+    {
+        $this->setExpectedException('Exception', 'Input file "/some-unreadable-file" not found');
+        $provider = new Provider();
+        $provider->setConfig(array(
+            'input' => 'some-unreadable-file'
+        ))->get();
+    }
+}
