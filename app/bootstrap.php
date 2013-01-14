8e1756ab0e6b7e21e3a1af55282501ef1c52698e
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-01-14 13:51:05 +0200

    Composer: Dynamic plugins dir adding to autoloader + boostraps.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,4 +2,7 @@
 $path = realpath(dirname(__FILE__) . '/../');
 set_include_path($path . PATH_SEPARATOR . get_include_path());
 
-require_once $path . '/vendor/autoload.php';
+//Return loader instance.
+require_once $path . '/Phrozn/Autoloader.php';
+
+return Phrozn\Autoloader::getInstance();
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,4 @@ use Phrozn\Autoloader as Loader;
 $path = realpath(dirname(__FILE__) . '/../');
 set_include_path($path . PATH_SEPARATOR . get_include_path());
 
-require_once $path . '/Phrozn/Autoloader.php';
-$loader = Loader::getInstance();
-$loader->getLoader(); // initialize autoloader
-
-// Twig uses perverted file naming (due to absense of NSs at a time it was written)
-// so fire up its own autoloader
-require_once $path . '/Phrozn/Vendor/Twig/Autoloader.php';
-\Twig_Autoloader::register();
+require_once $path . '/app/bootstrap.php';
